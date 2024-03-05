d9556005d975ed7a5936cc4513f6a616ed7c10f9
YUNDAEW0N <yundaewon343@gmail.com>
2024-03-05 13:15:55 +0000

    priority-condvar fin

--- threads/synch.c
+++ threads/synch.c
@@ -292,7 +292,8 @@ cond_wait (struct condition *cond, struct lock *lock) {
 	ASSERT (lock_held_by_current_thread (lock));
 
 	sema_init (&waiter.semaphore, 0);
-	list_push_back (&cond->waiters, &waiter.elem);
+	// list_push_back (&cond->waiters, &waiter.elem);
+	list_insert_ordered(&cond->waiters,&waiter.elem,cond_compare_priority,NULL);
 	lock_release (lock);
 	sema_down (&waiter.semaphore);
 	lock_acquire (lock);
@@ -312,9 +313,11 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED) {
 	ASSERT (!intr_context ());
 	ASSERT (lock_held_by_current_thread (lock));
 
-	if (!list_empty (&cond->waiters))
+	if (!list_empty (&cond->waiters)){
+		list_sort(&cond->waiters,cond_compare_priority,NULL);
 		sema_up (&list_entry (list_pop_front (&cond->waiters),
 					struct semaphore_elem, elem)->semaphore);
+	}
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
@@ -330,4 +333,20 @@ cond_broadcast (struct condition *cond, struct lock *lock) {
 
 	while (!list_empty (&cond->waiters))
 		cond_signal (cond, lock);
+}
+
+bool cond_compare_priority(const struct list_elem *a,const struct list_elem *b,void *aux)
+{
+	struct semaphore_elem *sema_elem_a = list_entry(a,struct semaphore_elem,elem);
+	struct semaphore_elem *sema_elem_b = list_entry(b,struct semaphore_elem,elem);
+
+	if(list_empty(&sema_elem_a->semaphore.waiters))
+		return false;
+	else if (list_empty(&sema_elem_b->semaphore.waiters))
+		return true;
+
+	struct thread *thread_a= list_entry(list_begin(&sema_elem_a->semaphore.waiters), struct thread, elem);
+	struct thread *thread_b= list_entry(list_begin(&sema_elem_b->semaphore.waiters), struct thread, elem);
+
+	return thread_a->priority > thread_b->priority;
 }
